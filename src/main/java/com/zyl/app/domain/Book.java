e93a5b584f5c4e67e27597aaeec2ee96f1e48afb
天川之约 <woaitianwen@qq.com>
2018-02-02 17:14:23 +0800

    Book实体类

---
+++ src/main/java/com/zyl/app/domain/Book.java
@@ -0,0 +1,51 @@
+package com.zyl.app.domain;
+
+public class Book {
+
+    private Long id;       //图书ID
+    private String title;  //图书名
+    private String author;  //图书作者
+
+    public Book() {
+        super();
+    }
+    public Book(Long id, String title, String author) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
